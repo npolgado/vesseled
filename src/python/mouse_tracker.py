fc52477e256590a154c8e3d7b7919aeed4c3deac
nicholas <nicholas831@gmail.com>
2024-03-13 18:52:49 -0700

    demo randomly on both strips

--- src/python/mouse_tracker.py
+++ src/python/mouse_tracker.py
@@ -2,8 +2,12 @@ import pyautogui
 import time
 import serial
 from serial import Serial
+from scipy.interpolate import interp1d
 
-RATE = 0.1
+top_strip_map_x = interp1d([1919,-1920],[0,144])
+top_strip_map_y = interp1d([0,1080],[0,255])
+
+RATE = 1
 command = "0x2E 0x09 \n"
 
 # print serial ports
@@ -13,36 +17,17 @@ print(ports)
 for port, desc, hwid in sorted(ports):
     print("{}: {} [{}]".format(port, desc, hwid))
 
-
-# ser = Serial(port="COM4", baudrate=115200, timeout=1, dtr=True)
-
-def send_command(command):
-    ser.write(command.encode('utf-8'))
-    time.sleep(1)
-    ser.close()
-    return "Command sent successfully"
-
 def get_cursor_position():
     # Get the current mouse position
     x, y = pyautogui.position()
     return x, y
 
 if __name__ == "__main__":
-
-    last_pos = False
-
     # Get Cursor Position
     x_coord, y_coord = get_cursor_position()
-
-    # Determine Monitor: x coord pos == mon 0
-
-    # 1 == TRUE
-    # 2 == FALSE
-    if x_coord > 0: last_pos = True
-    else: last_pos = False
         
     try:
-        with serial.Serial(port="COM4", baudrate=115200, timeout=0.05, dsrdtr=True) as ser:
+        with serial.Serial(port="COM4", baudrate=115200, timeout=0.05) as ser:
             # ser.write(serial.to_bytes(command.encode()))
             while True:
                 # print any received data
@@ -54,20 +39,15 @@ if __name__ == "__main__":
 
                 # Get Cursor Position
                 x_coord, y_coord = get_cursor_position()
-
-                # Determine Monitor: x coord pos == mon 0
-
-                # currently 1 but was 2
-                if x_coord > 0 and not last_pos: 
-                    ser.write("1".encode('utf-8'))
-                    last_pos = not last_pos
                 
-                # currently 2 but was 1
-                elif x_coord <= 0 and last_pos: 
-                    ser.write("2".encode('utf-8'))
-                    last_pos = not last_pos 
-                
+                top_strip_x = int(top_strip_map_x(x_coord))
+                top_strip_y = int(top_strip_map_y(y_coord))
+
+                # print(f"Top Strip X: {top_strip_x} Y: {top_strip_y}")       
+
+                ser.write(f"{top_strip_x}".encode('utf-8'))         
                 time.sleep(RATE)
+
     except Exception as e:
         print(e)
         exit()
